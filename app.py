e624fadd4728c4a15adf6c00baa63473dfa52840
Karlina05 <klina8486@gmail.com>
2023-10-12 00:29:13 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 
-client = MongoClient('mongodb://klina8486:Krlna.0192@ac-wugzihw-shard-00-00.uxglhio.mongodb.net:27017,ac-wugzihw-shard-00-01.uxglhio.mongodb.net:27017,ac-wugzihw-shard-00-02.uxglhio.mongodb.net:27017/?ssl=true&replicaSet=atlas-10nzre-shard-0&authSource=admin&retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
